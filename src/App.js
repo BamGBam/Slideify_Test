556b9fbb7ff5e4ea65a8c3f05206e32d1709b06d
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-05-16 17:43:23 -0700

    Finished first operations of REACT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile'; // Assuming the Profile component is in a file named Profile.js
+
+
+/////////////First function that they have///////////
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Sepehr <code> Joooon</code> 
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+////////////Making buttons
+// function MyButton() {
+//   return (
+//     <button>Generate New Ima</button>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <h1>Welcome to Slideify</h1>
+      <button className="button-color">Generate New Image</button>
+      <button>Generate second image</button>
     </div>
+    <div><Profile /> {/* hiiiiii */}</div>
+    <div>
+      <h1>Welcome to Slide2</h1>
+      <button>Generate New Image</button>
+      <button>Generate second image</button>
+    </div>
+  </div>
+    
   );
+  ///Adding photos
+  
 }
+
+
+
 
 export default App;
